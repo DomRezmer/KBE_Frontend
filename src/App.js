19627ec4989fd7b66f999c2d41b6df28a4bce7af
DomRezmer <dom.rezmer@gmail.com>
2023-08-02 16:57:10 +0200

    test: more merging

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-//TESTING DELETE LATER
+
 //styled components
 import { StyledContainer } from "./components/Styles";
 
